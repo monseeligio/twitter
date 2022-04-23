e496a18df45b2afad40f5bc886d226a0b2c5e9e4
monseeligio <monserrateligio@gmail.com>
2022-04-22 23:14:49 -0500

    agregando requrimiento 1 y 2

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,15 +1,25 @@
 const user =require('./../../app/models/user')
 
 describe ("prueba de jest",()=>{
+    
     test("prueba 1",()=>{
-        const User=new user(1,"monseeligio", "monserrat","bio", "dateCreated", "lastUpdate")
+        const User=new user(1,"monseeligio", "monserrat","bio")
         expect(User.id).toBe(1)
         expect(User.username).toBe("monseeligio")
+        expect(User.name).toBe("monserrat")
+        expect(User.bio).toBe("bio")
+
+
     })
 
     test("requerimiento 2: fechas en atributos de User,",()=>{
-        const User=new user(1,"monseeligio", "monserrat","bio", "dateCreated", "lastUpdate")
+        const User=new user(1,"monseeligio", "monserrat","bio")
+        expect(User.id).toBe(1)
+        expect(User.username).toBe("monseeligio")
+        expect(User.name).toBe("monserrat")
+        expect(User.bio).toBe("bio")
         expect(User.datecreated).not.toBeUndefined()
+        expect(User.lastUpdate).not.toBeUndefined()
     })
 
     test("requerimiento 3 : agregando getters,",()=>{
@@ -20,4 +30,5 @@ describe ("prueba de jest",()=>{
         const User=new user(1,"monseeligio", "monserrat","bio", "dateCreated", "lastUpdate")
         User.setUsername = "monseeligio"
     })
+
 })
